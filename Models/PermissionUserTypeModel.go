48ceae34fec1c845372845ad726239ca19c574d0
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:35:09 +0530

    Update PermissionUserTypeModel.go

--- Models/PermissionUserTypeModel.go
+++ Models/PermissionUserTypeModel.go
@@ -1,8 +1,8 @@
 package Models
 
 type PermissionUserType struct {
-	Id 					int 	`json:"_id"`
-	UserType  			string	`json:"user_type"`
+	Id 			int 	`json:"_id"`
+	UserType  		string	`json:"user_type"`
 	PermissionTypeId 	int 	`json:"permission_type_id"`
 }
 
