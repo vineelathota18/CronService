63731e0005515ce05b9bd50d516b5f91d651683e
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:35:33 +0530

    Update UserAuthenticationModel.go

--- Models/UserAuthenticationModel.go
+++ Models/UserAuthenticationModel.go
@@ -3,12 +3,12 @@ package Models
 import "time"
 
 type UserAuthentication struct{
-	Id				int 		`json:"_id"`
-	UserName		string		`json:"user_name"`
+	Id		int 		`json:"_id"`
+	UserName	string		`json:"user_name"`
 	HashPassword	string		`json:"hash_password"`
-	UserID			int			`json:"user_id"`
-	CreatedAt		time.Time	`json:"created_at"`
-	UpdatedAT		time.Time	`json:"updated_at"`
+	UserID		int			`json:"user_id"`
+	CreatedAt	time.Time	`json:"created_at"`
+	UpdatedAT	time.Time	`json:"updated_at"`
 }
 
 func ( ua *UserAuthentication) TableName() string {
