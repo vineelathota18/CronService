3fadb0035ee337e7a4c1d19b41fc436bfe7f2c12
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:35:48 +0530

    Update UserModel.go

--- Models/UserModel.go
+++ Models/UserModel.go
@@ -3,12 +3,12 @@ package Models
 import "time"
 
 type User struct{
-	Id 				int 		`json:"_id"`
+	Id 			int 		`json:"_id"`
 	UserType  		string		`json:"user_type"`
 	FirstName		string		`json:"first_name"`
 	LastName		string		`json:"last_name"`
 	Email			string		`json:"email"`
-	OrganisationId	int			`json:"organisation_id"`
+	OrganisationId		int			`json:"organisation_id"`
 	CreatedAt		time.Time	`json:"created_at"`
 	UpdatedAT		time.Time	`json:"updated_at"`
 }
