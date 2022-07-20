fca6d61fa28261e927ec479faa6aee1ef454342f
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:34:32 +0530

    Update OrganizationModel.go

--- Models/OrganizationModel.go
+++ Models/OrganizationModel.go
@@ -3,10 +3,10 @@ package Models
 import "time"
 
 type Organization struct{
-	Id 					int 		`json:"_id"`
+	Id 			int 		`json:"_id"`
 	OrganisationName	string		`json:"organisation_name"`
-	CreatedAt			time.Time	`json:"created_at"`
-	UpdatedAT			time.Time	`json:"updated_at"`
+	CreatedAt		time.Time	`json:"created_at"`
+	UpdatedAT		time.Time	`json:"updated_at"`
 }
 
 func (o *Organization) TableName() string {
