a74999b2eb5ec258cfdd65901bf6f553d41609cc
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:36:11 +0530

    Update UserTokenModel.go

--- Models/UserTokenModel.go
+++ Models/UserTokenModel.go
@@ -1,9 +1,9 @@
 package Models
 
 type UserToken struct {
-	Id					int 	`json:"_id"`
-	UserId				int		`json:"user_id"`
-	Token				string	`json:"token"`
+	Id			int 	`json:"_id"`
+	UserId			int	`json:"user_id"`
+	Token			string	`json:"token"`
 	TokenExpiryTime		string	`json:"token_expiry_time"`
 	TokenCreationTime	string	`json:"token_creation_time"`
 }
