312c1085cc9fd4a9ed5c0d74795ef92b005904d1
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:37:02 +0530

    Update UserAuthenticationModel.go

--- Models/UserAuthenticationModel.go
+++ Models/UserAuthenticationModel.go
@@ -6,7 +6,7 @@ type UserAuthentication struct{
 	Id		int 		`json:"_id"`
 	UserName	string		`json:"user_name"`
 	HashPassword	string		`json:"hash_password"`
-	UserID		int			`json:"user_id"`
+	UserID		int		`json:"user_id"`
 	CreatedAt	time.Time	`json:"created_at"`
 	UpdatedAT	time.Time	`json:"updated_at"`
 }
