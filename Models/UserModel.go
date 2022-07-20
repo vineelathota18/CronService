2d535069ec36f6ebb08f4a77ceb7503333956b69
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:37:13 +0530

    Update UserModel.go

--- Models/UserModel.go
+++ Models/UserModel.go
@@ -8,7 +8,7 @@ type User struct{
 	FirstName		string		`json:"first_name"`
 	LastName		string		`json:"last_name"`
 	Email			string		`json:"email"`
-	OrganisationId		int			`json:"organisation_id"`
+	OrganisationId		int		`json:"organisation_id"`
 	CreatedAt		time.Time	`json:"created_at"`
 	UpdatedAT		time.Time	`json:"updated_at"`
 }
