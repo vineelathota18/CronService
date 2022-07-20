85f5ea4e4fbf92b37abb5caaa5a1eae934aade6a
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:34:16 +0530

    Update CronExecutionResultModel.go

--- Models/CronExecutionResultModel.go
+++ Models/CronExecutionResultModel.go
@@ -6,12 +6,12 @@ import (
 
 type CronExecutionResult struct {
 	CronJobId  		string		`json:"cron_job_id"`
-	URL				string		`json:"url"`
+	URL			string		`json:"url"`
 	Output			string		`json:"output"`
 	Error			string		`json:"error"`
 	Time			time.Time	`json:"time"`
 	StartTime		time.Time	`json:"start_time"`
-	ExecutionTime	time.Time	`json:"execution_time"`
+	ExecutionTime		time.Time	`json:"execution_time"`
 	Status			int 		`json:"status"`
 }
 
