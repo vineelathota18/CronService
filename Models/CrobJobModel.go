69a032590503e907c207c1b3dcd3a862fbdbbe93
Vineela Thota <77935110+vineelathota18@users.noreply.github.com>
2022-07-20 11:33:31 +0530

    Update CrobJobModel.go

--- Models/CrobJobModel.go
+++ Models/CrobJobModel.go
@@ -5,10 +5,10 @@ import (
 )
 
 type CronJob struct {
-	Id 				int 		`json:"_id"`
+	Id 			int 		`json:"_id"`
 	Name			string		`json:"name"`
 	Expression		string		`json:"expression"`
-	URL				string		`json:"url"`
+	URL			string		`json:"url"`
 	Timezone		string		`json:"timezone"`
 	HttpMethod		string		`json:"http_method"`
 	HttpHeader		string		`json:"http_header"`
